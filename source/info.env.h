b87db4423a483bc217e5727a99dc4b6ebdaea711
HaoCoder-2007 <trangiahao0703@gmail.com>
2026-05-28 10:44:34 +0700

    Update

--- source/info.env.h
+++ source/info.env.h
@@ -1,7 +1,8 @@
-// #define SECRET_SSID_1 "Your_SSID"
-// #define SECRET_PASS_1 "Your_Password"
+// -Create a file: info.dev.h and define all your SSID pair and API KEY
+#define SECRET_SSID_1 "Your_SSID"
+#define SECRET_PASS_1 "Your_Password"
 // ...
 
-// #define SECRET_API_KEY "Your_API_KEY"
-// -Creat an account on 'openweathermap.org'
-// -Creat your API
+#define SECRET_API_KEY "Your_API_KEY"
+// -Create an account on 'openweathermap.org'
+// -Create your API
